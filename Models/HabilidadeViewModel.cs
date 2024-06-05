1b167521c3f71d2355d388feab51727309f86f52
Bruno <brunohmfcorporativo@gmail.com>
2024-06-04 22:36:23 -0300

    feat: update

---
+++ Models/HabilidadeViewModel.cs
@@ -0,0 +1,11 @@
+namespace RpgMvc.Models
+{
+    public class HabilidadeViewModel
+    {
+        public int id {get; set;}
+        public string nome { get; set; } = string.Empty;
+
+        public int dano { get; set; } = 0;
+        
+    }
+}
---
+++ Views/Personagens/Index.cs
@@ -0,0 +1,3 @@
+@Html.ActionLink("Clique aqui para um embate com armas", "Index", "Disputas", null, new {@class = "btn btn-warning "})
+
+@Html.ActionLink("Clique aqui para um embate com Habilidades", "IndexHabilidades", "Disputas", null, new {@class = "btn btn-dark "})
